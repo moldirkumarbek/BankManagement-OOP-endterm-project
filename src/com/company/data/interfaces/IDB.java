21e9f253cceb5ffde1c4dcb9ae10092c20cf1263
Moldir Kumarbek <molderhumarbek@gmail.com>
2021-03-05 13:48:59 +0600

    Maryam's part

---
+++ src/com/company/data/interfaces/IDB.java
@@ -0,0 +1,11 @@
+package com.company.data.interfaces;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Interface class IDB that has getConnection method
+ */
+public interface IDB {
+    Connection getConnection() throws SQLException, ClassNotFoundException;
+}
